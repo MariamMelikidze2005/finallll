f87e82e5104002813d5b9ae3efd9388c7693ac35
KhatoMelikidze <x.meliqidze98@gmail.com>
2025-03-29 09:07:03 +0400

    adding

---
+++ forfinalofC#/Form1.Designer.cs
@@ -0,0 +1,263 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace forfinalofC_
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboAuthors = new System.Windows.Forms.ComboBox();
+            this.comboBooks = new System.Windows.Forms.ComboBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtPublishedYear = new System.Windows.Forms.TextBox();
+            this.txtPrice = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnLoadBooks = new System.Windows.Forms.Button();
+            this.btnUpdateBook = new System.Windows.Forms.Button();
+            this.btnInsertBook = new System.Windows.Forms.Button();
+            this.btnDeleteBook = new System.Windows.Forms.Button();
+            this.txtTitle = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // comboAuthors
+            // 
+            this.comboAuthors.FormattingEnabled = true;
+            this.comboAuthors.Location = new System.Drawing.Point(43, 84);
+            this.comboAuthors.Name = "comboAuthors";
+            this.comboAuthors.Size = new System.Drawing.Size(121, 21);
+            this.comboAuthors.TabIndex = 0;
+            this.comboAuthors.SelectedIndexChanged += new System.EventHandler(this.comboAuthors_SelectedIndexChanged);
+            // 
+            // comboBooks
+            // 
+            this.comboBooks.FormattingEnabled = true;
+            this.comboBooks.Location = new System.Drawing.Point(210, 84);
+            this.comboBooks.Name = "comboBooks";
+            this.comboBooks.Size = new System.Drawing.Size(121, 21);
+            this.comboBooks.TabIndex = 1;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(407, 54);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(361, 361);
+            this.dataGridView1.TabIndex = 2;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(51, 58);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(98, 24);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "for authors";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(221, 58);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(87, 24);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "for books";
+            // 
+            // txtPublishedYear
+            // 
+            this.txtPublishedYear.Location = new System.Drawing.Point(210, 271);
+            this.txtPublishedYear.Name = "txtPublishedYear";
+            this.txtPublishedYear.Size = new System.Drawing.Size(100, 20);
+            this.txtPublishedYear.TabIndex = 4;
+            // 
+            // txtPrice
+            // 
+            this.txtPrice.Location = new System.Drawing.Point(210, 240);
+            this.txtPrice.Name = "txtPrice";
+            this.txtPrice.Size = new System.Drawing.Size(100, 20);
+            this.txtPrice.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(39, 266);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(143, 24);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Published Year:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(124, 235);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(58, 24);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Price:";
+            // 
+            // btnLoadBooks
+            // 
+            this.btnLoadBooks.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.btnLoadBooks.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLoadBooks.Location = new System.Drawing.Point(43, 311);
+            this.btnLoadBooks.Name = "btnLoadBooks";
+            this.btnLoadBooks.Size = new System.Drawing.Size(121, 38);
+            this.btnLoadBooks.TabIndex = 5;
+            this.btnLoadBooks.Text = "Load Books";
+            this.btnLoadBooks.UseVisualStyleBackColor = false;
+            this.btnLoadBooks.Click += new System.EventHandler(this.btnLoadBooks_Click);
+            // 
+            // btnUpdateBook
+            // 
+            this.btnUpdateBook.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.btnUpdateBook.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnUpdateBook.Location = new System.Drawing.Point(43, 364);
+            this.btnUpdateBook.Name = "btnUpdateBook";
+            this.btnUpdateBook.Size = new System.Drawing.Size(121, 38);
+            this.btnUpdateBook.TabIndex = 5;
+            this.btnUpdateBook.Text = "Update Book";
+            this.btnUpdateBook.UseVisualStyleBackColor = false;
+            this.btnUpdateBook.Click += new System.EventHandler(this.btnUpdateBook_Click);
+            // 
+            // btnInsertBook
+            // 
+            this.btnInsertBook.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.btnInsertBook.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnInsertBook.Location = new System.Drawing.Point(208, 311);
+            this.btnInsertBook.Name = "btnInsertBook";
+            this.btnInsertBook.Size = new System.Drawing.Size(121, 38);
+            this.btnInsertBook.TabIndex = 5;
+            this.btnInsertBook.Text = "Insert Book";
+            this.btnInsertBook.UseVisualStyleBackColor = false;
+            this.btnInsertBook.Click += new System.EventHandler(this.btnInsertBook_Click);
+            // 
+            // btnDeleteBook
+            // 
+            this.btnDeleteBook.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.btnDeleteBook.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDeleteBook.Location = new System.Drawing.Point(208, 364);
+            this.btnDeleteBook.Name = "btnDeleteBook";
+            this.btnDeleteBook.Size = new System.Drawing.Size(121, 38);
+            this.btnDeleteBook.TabIndex = 5;
+            this.btnDeleteBook.Text = "Delete Book";
+            this.btnDeleteBook.UseVisualStyleBackColor = false;
+            this.btnDeleteBook.Click += new System.EventHandler(this.btnDeleteBook_Click);
+            // 
+            // txtTitle
+            // 
+            this.txtTitle.Location = new System.Drawing.Point(210, 205);
+            this.txtTitle.Name = "txtTitle";
+            this.txtTitle.Size = new System.Drawing.Size(100, 20);
+            this.txtTitle.TabIndex = 6;
+            this.txtTitle.TextChanged += new System.EventHandler(this.txtTitle_TextChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(140, 200);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(42, 24);
+            this.label5.TabIndex = 3;
+            this.label5.Text = "title:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtTitle);
+            this.Controls.Add(this.btnInsertBook);
+            this.Controls.Add(this.btnDeleteBook);
+            this.Controls.Add(this.btnUpdateBook);
+            this.Controls.Add(this.btnLoadBooks);
+            this.Controls.Add(this.txtPrice);
+            this.Controls.Add(this.txtPublishedYear);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.comboBooks);
+            this.Controls.Add(this.comboAuthors);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+     
+
+    
+
+
+        
+
+
+
+   
+
+
+
+
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboAuthors;
+        private System.Windows.Forms.ComboBox comboBooks;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtPublishedYear;
+        private System.Windows.Forms.TextBox txtPrice;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnLoadBooks;
+        private System.Windows.Forms.Button btnUpdateBook;
+        private System.Windows.Forms.Button btnInsertBook;
+        private System.Windows.Forms.Button btnDeleteBook;
+        private System.Windows.Forms.TextBox txtTitle;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ forfinalofC#/Form1.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace forfinalofC_
+{
+    public partial class Form1 : Form
+    {
+        private readonly string connectionString = "Server=DESKTOP-JFOLDL4\\SQLEXPRESS;Database=LibraryDB;Integrated Security=True;";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void Form1_Load(object sender, EventArgs e)
+        {
+            await LoadAuthors();
+            await LoadBooks();
+        }
+
+        private async Task LoadAuthors()
+        {
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                await conn.OpenAsync();
+                using (SqlCommand cmd = new SqlCommand("SELECT DISTINCT AuthorId, AuthorName FROM Authors", conn))
+                using (SqlDataReader reader = await cmd.ExecuteReaderAsync())
+                {
+                    DataTable dt = new DataTable();
+                    dt.Load(reader);
+
+                    foreach (DataColumn col in dt.Columns)
+                    {
+                        Console.WriteLine(col.ColumnName);
+                    }
+
+                    if (dt.Columns.Contains("Name"))
+                    {
+                        comboAuthors.DataSource = dt;
+                        comboAuthors.DisplayMember = "Name";  
+                        comboAuthors.ValueMember = "AuthorId";
+                        comboAuthors.SelectedIndex = -1;
+                    }
+                    else
+                    {
+                        MessageBox.Show("AuthorName column not found in database results.");
+                    }
+                }
+            }
+        }
+
+        private async Task LoadBooks()
+        {
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                await conn.OpenAsync();
+                using (SqlCommand cmd = new SqlCommand("SELECT * FROM Books", conn))
+                using (SqlDataReader reader = await cmd.ExecuteReaderAsync())
+                {
+                    DataTable dt = new DataTable();
+                    dt.Load(reader);
+                    dataGridView1.DataSource = dt;
+                    comboBooks.DataSource = dt;
+                    comboBooks.DisplayMember = "Title";
+                    comboBooks.ValueMember = "BookId";
+                    comboBooks.SelectedIndex = -1;
+                }
+            }
+        }
+
+        private async void btnLoadBooks_Click(object sender, EventArgs e)
+        {
+            await LoadBooks();
+        }
+
+        private async void btnInsertBook_Click(object sender, EventArgs e)
+        {
+            if (comboAuthors.SelectedValue == null || string.IsNullOrWhiteSpace(txtTitle.Text) ||
+                string.IsNullOrWhiteSpace(txtPublishedYear.Text) || string.IsNullOrWhiteSpace(txtPrice.Text))
+            {
+                MessageBox.Show("Please fill in all fields and select an author.");
+                return;
+            }
+
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                await conn.OpenAsync();
+                using (SqlCommand cmd = new SqlCommand("InsertBook", conn))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@Title", txtTitle.Text);
+                    cmd.Parameters.AddWithValue("@AuthorId", comboAuthors.SelectedValue);
+                    cmd.Parameters.AddWithValue("@PublishedYear", Convert.ToInt32(txtPublishedYear.Text));
+                    cmd.Parameters.AddWithValue("@Price", Convert.ToDecimal(txtPrice.Text));
+
+                    await cmd.ExecuteNonQueryAsync();
+                }
+            }
+            MessageBox.Show("Book Inserted Successfully!");
+            await LoadBooks();
+        }
+
+        private async void btnUpdateBook_Click(object sender, EventArgs e)
+        {
+            if (comboBooks.SelectedValue == null)
+            {
+                MessageBox.Show("Please select a book to update.");
+                return;
+            }
+
+            if (comboAuthors.SelectedValue == null)
+            {
+                MessageBox.Show("Please select an author.");
+                return;
+            }
+
+            int authorId;
+            if (!int.TryParse(comboAuthors.SelectedValue.ToString(), out authorId))
+            {
+                MessageBox.Show("Invalid Author ID.");
+                return;
+            }
+
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                await conn.OpenAsync();
+                using (SqlCommand cmd = new SqlCommand("UpdateBook", conn))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@BookId", comboBooks.SelectedValue);
+                    cmd.Parameters.AddWithValue("@Title", txtTitle.Text);
+                    cmd.Parameters.AddWithValue("@AuthorId", authorId); 
+                    cmd.Parameters.AddWithValue("@PublishedYear", Convert.ToInt32(txtPublishedYear.Text));
+                    cmd.Parameters.AddWithValue("@Price", Convert.ToDecimal(txtPrice.Text));
+
+                    await cmd.ExecuteNonQueryAsync();
+                }
+            }
+            MessageBox.Show("Book Updated Successfully!");
+            await LoadBooks();
+        }
+
+
+        private async void btnDeleteBook_Click(object sender, EventArgs e)
+        {
+            if (comboBooks.SelectedValue == null)
+            {
+                MessageBox.Show("Please select a book to delete.");
+                return;
+            }
+
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                await conn.OpenAsync();
+                using (SqlCommand cmd = new SqlCommand("DeleteBook", conn))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@BookId", comboBooks.SelectedValue);
+
+                    await cmd.ExecuteNonQueryAsync();
+                }
+            }
+            MessageBox.Show("Book Deleted Successfully!");
+            await LoadBooks();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count > 0)
+            {
+                var row = dataGridView1.SelectedRows[0];
+
+                txtTitle.Text = row.Cells["Title"].Value.ToString();
+                comboAuthors.SelectedValue = row.Cells["AuthorId"].Value; 
+                txtPublishedYear.Text = row.Cells["PublishedYear"].Value.ToString();
+                txtPrice.Text = row.Cells["Price"].Value.ToString();
+            }
+        }
+
+        private void comboAuthors_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // You can implement filtering books based on author selection here if needed.
+        }
+
+        private void txtTitle_TextChanged(object sender, EventArgs e)
+        {
+        }
+    }
+}
